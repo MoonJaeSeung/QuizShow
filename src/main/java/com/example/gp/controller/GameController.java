444ae6e5adcc9290510c70c63ad78ae23b37e30b
MoonJaeSeung <wotmdskfk32@naver.com>
2023-06-30 21:24:16 +0900

    fix : game view 로직 수정

--- src/main/java/com/example/gp/controller/GameController.java
+++ src/main/java/com/example/gp/controller/GameController.java
@@ -30,31 +30,11 @@ public class GameController {
     @GetMapping("/game")
     public String gameView(Model model, HttpSession session){
         List<Word> words = gameService.findAll();
-
-        Integer currentWordIndex = (Integer) session.getAttribute("currentWordIndex");
-        if (currentWordIndex == null || currentWordIndex >= words.size()) {
-            currentWordIndex = 0; // start over when we reach the end of the list
-        }
-
-        Word currentWord = words.get(currentWordIndex);
-        model.addAttribute("currentWord", currentWord);
-        session.setAttribute("currentWordIndex", currentWordIndex + 1); // increment the index for the next request
+        model.addAttribute("words", words);
 
         return "game/gameView";
     }
 
-    @PostMapping("/guess")
-    public String guess(@RequestParam("wordId") Long wordId, @RequestParam("guess") String guess) {
-        Word word = gameService.findWordById(wordId);
-        if (word != null) {
-            String remainingLetters = word.getFullWord().substring(2);
-            if (remainingLetters.equals(guess)) {
-                // the guess was correct, do something here if necessary
-            }
-        }
-
-        return "redirect:/game"; // redirect back to the game view to show the next word
-    }
 
     @GetMapping("/addWord")
     public String addWord(){
@@ -69,58 +49,56 @@ public class GameController {
         System.out.println("savedWord = " + savedWord);
         return "/admin/addWord";
     }
-
-
-
-    @PostMapping("/celeb/add")
-    public String addCelebrity(@RequestParam("name") String name, @RequestParam("itemImgFile") MultipartFile file) throws Exception {
-        if(!file.isEmpty()){
-            try{
-                //파일 이름 가지고 오기
-                String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-
-                // 저장 경로 설정
-                String uploadDir = "C:/shop/items";
-
-                // 파일 저장 경로가 존재하지 않는다면 생성
-                Path uploadPath = Paths.get(uploadDir);
-                if(!Files.exists(uploadPath)){
-                    Files.createDirectories(uploadPath);
-                }
-
-                // 지정한 경로에 파일을 저장
-                Path filePath = uploadPath.resolve(fileName);
-                file.transferTo(filePath.toFile());
-
-                return "gameView";
-
-            }catch(Exception e){
-
-            }
-        }
-
-        return "error";
-    }
-
-    @GetMapping("/game/allPhotos")
-    public String getAllPhotos(Model model){
-        //경로에 있는 모든 사진을 가져오기
-        File uploadFolder = new File("C:/shop/items");
-        File[] files = uploadFolder.listFiles();
-
-        List<String> photoFileNames = new ArrayList<>();
-
-        // 파일 이름을 리스트에 넣기
-        if(files != null){
-            for(File file : files){
-                photoFileNames.add(file.getName());
-            }
-        }
-        System.out.println("photoFileNames = " + photoFileNames);
-        model.addAttribute("photoFileNames", photoFileNames);
-
-        return "gameView";
-    }
-
-
 }
+
+
+
+//    @PostMapping("/celeb/add")
+//    public String addCelebrity(@RequestParam("name") String name, @RequestParam("itemImgFile") MultipartFile file) throws Exception {
+//        if(!file.isEmpty()){
+//            try{
+//                //파일 이름 가지고 오기
+//                String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+//
+//                // 저장 경로 설정
+//                String uploadDir = "C:/shop/items";
+//
+//                // 파일 저장 경로가 존재하지 않는다면 생성
+//                Path uploadPath = Paths.get(uploadDir);
+//                if(!Files.exists(uploadPath)){
+//                    Files.createDirectories(uploadPath);
+//                }
+//
+//                // 지정한 경로에 파일을 저장
+//                Path filePath = uploadPath.resolve(fileName);
+//                file.transferTo(filePath.toFile());
+//
+//                return "gameView";
+//
+//            }catch(Exception e){
+//
+//            }
+//        }
+//
+//        return "error";
+//    }
+//
+//    @GetMapping("/game/allPhotos")
+//    public String getAllPhotos(Model model){
+//        //경로에 있는 모든 사진을 가져오기
+//        File uploadFolder = new File("C:/shop/items");
+//        File[] files = uploadFolder.listFiles();
+//
+//        List<String> photoFileNames = new ArrayList<>();
+//
+//        // 파일 이름을 리스트에 넣기
+//        if(files != null){
+//            for(File file : files){
+//                photoFileNames.add(file.getName());
+//            }
+//        }
+//        System.out.println("photoFileNames = " + photoFileNames);
+//        model.addAttribute("photoFileNames", photoFileNames);
+//
+//        return "gameView";
+//    }
