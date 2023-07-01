71aeece1640be1e1e865664ccc5d811226a2dd8b
MoonJaeSeung <wotmdskfk32@naver.com>
2023-07-01 15:40:30 +0900

    feat : header 문구 적용

--- src/main/java/com/example/gp/controller/MainController.java
+++ src/main/java/com/example/gp/controller/MainController.java
@@ -15,4 +15,9 @@ public class MainController {
     public String admin(){
         return "admin";
     }
+
+    @GetMapping("/choice")
+    public String choice(){
+        return "choice";
+    }
 }
