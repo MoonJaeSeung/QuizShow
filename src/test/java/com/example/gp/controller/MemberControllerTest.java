f0cbda19dd7a28dd8e9eb11dce17516459953e5f
MoonJaeSeung <wotmdskfk32@naver.com>
2023-07-17 17:24:04 +0900

    style : css 파일 1차 분리

---
+++ src/test/java/com/example/gp/controller/MemberControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.gp.controller;
+
+import com.example.gp.controller.MemberController;
+import com.example.gp.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class MemberControllerTest {
+
+
+}
