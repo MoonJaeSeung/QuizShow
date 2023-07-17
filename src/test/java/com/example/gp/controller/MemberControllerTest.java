acb2657268dd6b286aca76977cb428a3ee42e36e
MoonJaeSeung <wotmdskfk32@naver.com>
2023-07-18 06:10:48 +0900

    style : code 정리

--- src/test/java/com/example/gp/controller/MemberControllerTest.java
+++ src/test/java/com/example/gp/controller/MemberControllerTest.java
@@ -1,26 +1,14 @@
 package com.example.gp.controller;
 
-import com.example.gp.controller.MemberController;
-import com.example.gp.dto.MemberFormDto;
-import com.example.gp.entity.Member;
-import com.example.gp.security.PasswordEncoder;
-import com.example.gp.service.MemberService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
+
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,22 +16,8 @@ import static org.mockito.Mockito.when;
 @TestPropertySource(locations = "classpath:application-test.yaml")
 public class MemberControllerTest {
 
-    @Autowired
-    private MemberService memberService;
 
-    @Autowired
-    private MockMvc mockMvc;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    public Member createMember(String nick, String password) {
-        MemberFormDto memberFormDto = new MemberFormDto();
-        memberFormDto.setNick(nick);
-        memberFormDto.setPassword(password);
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        return memberService.saveMember(member);
-    }
 
 
 
