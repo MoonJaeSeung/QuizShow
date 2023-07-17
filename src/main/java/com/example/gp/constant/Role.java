33c12347f1252e380a1170aefac43ea7c543be30
MoonJaeSeung <wotmdskfk32@naver.com>
2023-07-18 05:24:08 +0900

    style : code 정리

--- src/main/java/com/example/gp/constant/Role.java
+++
@@ -1,8 +0,0 @@
-package com.example.gp.constant;
-
-import lombok.Getter;
-
-@Getter
-public enum Role {
-    USER, ADMIN
-}
--- src/main/java/com/example/gp/entity/Member.java
+++ src/main/java/com/example/gp/entity/Member.java
@@ -1,6 +1,6 @@
 package com.example.gp.entity;
 
-import com.example.gp.constant.Role;
+
 import com.example.gp.dto.MemberFormDto;
 import lombok.*;
 
@@ -22,20 +22,11 @@ public class Member {
 
     private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
-    public Member(String nick, String password, Role role) {
-        this.nick = nick;
-        this.password = password;
-        this.role = role;
-    }
 
     public static Member createMember(MemberFormDto memberFormDto, String encodedPassword){
         Member member = new Member();
         member.setNick(memberFormDto.getNick());
         member.setPassword(encodedPassword);
-        member.setRole(Role.USER);
         return member;
     }
 }
